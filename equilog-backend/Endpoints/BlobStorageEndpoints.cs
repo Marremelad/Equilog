3481568666fd731c589ccfcf85eb9f84abb68a6c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-25 19:41:29 +0200

    Added comments to BlobStorageEndpoints.cs

--- equilog-backend/Endpoints/BlobStorageEndpoints.cs
+++ equilog-backend/Endpoints/BlobStorageEndpoints.cs
@@ -7,12 +7,15 @@ public class BlobStorageEndpoints
 {
     public static void RegisterEndpoints(WebApplication app)
     {
+        // Get upload uri.
         app.MapGet("/api/blob-storage/get-upload-uri", GetUploadUri) // "/api/blobs/{blobName}/upload-uri"
             .WithName("GetUploadUri");
 
+        // Get read uri.
         app.MapGet("/api/blob-storage/get-read-uri", GetReadUri) // "/api/blobs/{blobName}/read-uri"
             .WithName("GetReadUri");
 
+        // Delete blob from blob storage.
         app.MapDelete("/api/blob-storage/delete-blob", DeleteBlob) // "/api/blobs/{blobName}"
             .WithName("DeleteBlob");
     }
